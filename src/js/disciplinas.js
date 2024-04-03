30ef103bca4eca95caf9d76417ae65af86c21ff0
Dizeu <122738432+DizeuM@users.noreply.github.com>
2024-04-02 22:45:46 -0300

    troca de "id" por "periodo" e ajustes de indentação

--- src/js/disciplinas.js
+++ src/js/disciplinas.js
@@ -9,37 +9,38 @@ function carregarJSON(url) {
       .catch(error => {
         console.error('Erro:', error);
       });
-  }
+}
   
-  function listarDisciplinasPorIdETipo(periodos, tipo, id) {
-    return periodos[id][tipo].disciplinas;
-  }
+function listarDisciplinasPorIdETipo(periodos, tipo, periodo) {
+  return periodos[periodo][tipo].disciplinas;
+}
   
-  function criarBotoesDisciplinas(disciplinas) {
-    const disciplinasContainer = document.getElementById('disciplinas-container');
-  
-    disciplinas.forEach(disciplina => {
-      const botaoDisciplina = document.createElement('button');
-      botaoDisciplina.className = 'btn btn-primary btn-lg w-100 mb-3';
-      botaoDisciplina.textContent = disciplina.nome;
-  
-      botaoDisciplina.addEventListener('click', () => {
-        alert(`Você clicou na disciplina ${disciplina.nome}`);
-      });
+function criarBotoesDisciplinas(disciplinas) {
+  const disciplinasContainer = document.getElementById('disciplinas-container');
 
-      disciplinasContainer.appendChild(botaoDisciplina);
+  disciplinas.forEach(disciplina => {
+    const botaoDisciplina = document.createElement('button');
+    botaoDisciplina.className = 'btn btn-primary btn-lg w-100 mb-3';
+    botaoDisciplina.textContent = disciplina.nome;
+    botaoDisciplina.setAttribute('data-disciplina-id', disciplina.id);
+
+    botaoDisciplina.addEventListener('click', () => {
+      const disciplinaId = botaoDisciplina.getAttribute('data-disciplina-id');
+      window.location.href = `/src/pages/disciplinas/detalhes.html?tipo=${tipo}&periodo=${periodo}&disciplina=${disciplinaId}`;
     });
-  }
+
+    disciplinasContainer.appendChild(botaoDisciplina);
+  });
+}
   
-  const urlJSON = '/src/data/disciplinas.json';
-  
-  const urlParams = new URLSearchParams(window.location.search);
-  const tipo = urlParams.get('tipo');
-  const id = parseInt(urlParams.get('id'));
-  
-  carregarJSON(urlJSON)
-    .then(data => {
-      const disciplinas = listarDisciplinasPorIdETipo(data.periodos, tipo, id);
-      criarBotoesDisciplinas(disciplinas);
-    });
+const urlJSON = '/src/data/disciplinas.json';
+
+const urlParams = new URLSearchParams(window.location.search);
+const tipo = urlParams.get('tipo');
+const periodo = parseInt(urlParams.get('periodo'));
+
+carregarJSON(urlJSON).then(data => {
+  const disciplinas = listarDisciplinasPorIdETipo(data.periodos, tipo, periodo);
+  criarBotoesDisciplinas(disciplinas);
+});
   
