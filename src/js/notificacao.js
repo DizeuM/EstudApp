de7d75e718ae0d2b96c98d7ca5e6493e93cdc44d
Dizeu <122738432+DizeuM@users.noreply.github.com>
2024-04-15 10:48:24 -0300

    notificacoes

--- src/js/notificacao.js
+++ src/js/notificacao.js
@@ -1,4 +1,3 @@
-
 function requestPermission() {
   if (!('Notification' in window)) {
     alert('Notification API not supported!');
@@ -6,4 +5,17 @@ function requestPermission() {
   }
   
   Notification.requestPermission();
+}
+
+function nonPersistentNotification() {
+  if (!('Notification' in window)) {
+    alert('Notification API not supported!');
+    return;
+  }
+  
+  try {
+    var notification = new Notification("Aprensentação de UX - Hoje às 09:40 na sala 404");
+  } catch (err) {
+    alert('Notification API error: ' + err);
+  }
 }
